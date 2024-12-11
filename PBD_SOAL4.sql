d6a297b27c6e4c17a756f5b61b0f9915f7872826
twenteazfour <ellenaplidazalni@gmail.com>
2024-12-11 23:41:19 +0700

    Delete PBD_SOAL4.sql

--- PBD_SOAL4.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	Sales.Customers.custid,
-	Sales.Customers.companyname,
-	Sales.Orders.orderid,
-	Sales.Orders.orderdate
-FROM Sales.Customers AS
-LEFT JOIN 
-ON Sales.Customers.custid = Sales.Orders.custid;
