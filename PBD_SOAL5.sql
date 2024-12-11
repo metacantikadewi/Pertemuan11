16fbb7de8fcb90da14f33486e889c6181f7266f6
Ellen Aplida Zalni <ellenaplidazalni@gmail.com>
2024-12-11 19:37:46 +0700

    upload

--- PBD_SOAL5.sql
+++
@@ -1,4 +0,0 @@
-SELECT Sales.Orders.orderid, Sales.Orders.orderdate, Sales.Customers.custid, Sales.Orders.empid
-FROM Sales.Orders
-WHERE
-	orderdate = (SELECT MAX(orderdate) FROM Sales.Orders);
--- PBD_SOAL6.sql
+++
@@ -1,6 +0,0 @@
-SELECT HR.Employees.empid, HR.Employees.FirstName, HR.Employees.LastName
-FROM HR.Employees
-LEFT JOIN Sales.Orders
-ON HR.Employees.empid =	Sales.Orders.empid
-WHERE Sales.OrderDate < '2016-05-01' 
-OR Sales.OrderDate IS NULL;
